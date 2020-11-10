caad9c67e6236e67b62b98fd64d8278d14f828a0
Akilah Rabb <akilahrabb@gmail.com>
2020-11-10 12:49:35 -0500

    added more comments

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,12 @@ public class Main {
     public static void main(String[] args) {
         // write your code here
         System.out.println("Hello");
+
+        // I have already published on Githum.com
+        // now I am ready to make more commits
+        // but do i need to share again?
+        // ABSOLUTELY NOT
+        // the project is already shared
+        // It's time to commit again
     }
 }
